fe96851009c06e27f547d823eeaca37afc88eb1c
Commondore <alex.som86@gmail.com>
2025-02-06 20:02:25 +0600

    add change name func

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import { Header } from "./components/header";
 import { Person } from "./components/person";
 import { Title } from "./components/title";
 import { Counter } from "./components/counter";
+import { useState } from "react";
 
 const App = () => {
+  const [users, setUsers] = useState([
+    { id: 1, name: "Mike", age: 20, avatar: "/images/profile.png", hobby: "Шахматы" },
+    { id: 2, name: "John", age: 30, avatar: "/images/profile.png", hobby: "Баскетбол" },
+  ]);
+
+  const changeName = () => {
+    const copy = [...users];
+    const person = { ...users[0] };
+    person.name = "Peter Parker";
+    copy[0] = person;
+    setUsers(copy);
+  };
+
   return (
     <>
       <Header />
@@ -12,13 +26,14 @@ const App = () => {
         <div className="text-center">
           <Title text="Приложение на React" />
           <Counter />
+          <button onClick={changeName}>Изменить имя 1го пользователя</button>
         </div>
         <main className="posts">
-          <Person name="Mike" age={20} image="/images/profile.png">
-            <p>Хобби: Шахматы</p>
+          <Person name={users[0].name} age={users[0].age} image={users[0].avatar}>
+            <p>Хобби: {users[0].hobby}</p>
           </Person>
-          <Person name="John" age={30} image="/images/profile2.webp">
-            <p>Хобби: Баскетбол</p>
+          <Person name={users[1].name} age={users[1].age} image={users[1].avatar}>
+            <p>Хобби: {users[1].hobby}</p>
           </Person>
         </main>
       </div>
